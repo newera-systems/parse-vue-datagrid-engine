a2042e67b436d34ce935c89c7b2f172577b94695
Josias Kasongo <josiaskas@gmail.com>
2023-03-06 15:04:23 -0500

    update export

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'DataGridBVue',
-      formats: ['es'],
+      formats: ['es', 'cjs'],
       fileName: 'datagrid-bvue',
     },
     rollupOptions: {
