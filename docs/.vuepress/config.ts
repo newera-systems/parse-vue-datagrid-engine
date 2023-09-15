2573b1dafeab8cb9822df991d3d42b422ab862ab
Josias Kasongo <josiaskas@gmail.com>
2023-09-15 12:12:13 -0400

    start docs with vue press

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,13 @@
+export default {
+    title: 'Datagrid-bvue',
+    description: 'Documentation for Datagrid-bvue',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            // other navigation links can be added here
+        ],
+        sidebar: [
+            // Sidebar contents can be added here
+        ]
+    }
+}
